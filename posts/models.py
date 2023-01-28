637523d2b1ffc07dc815fae4e263c9b9866e428e
Jody M <jodyamm@hotmail.com>
2023-01-28 10:48:27 +0000

    Add image_filter_choices to model in posts app

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,36 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+
+    image_filter_choices = [
+        ('NO_FILTER', 'no filter'),
+        ('COLORIZED', 'colorized'),
+        ('GRAYSCALE', 'grayscale'),
+        ('BLURRED', 'blurred'),
+        ('BINARY', 'binary'),
+        ('INVERT', 'invert'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II'),
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(
         upload_to='images/', default='../default_hl6wjy.png', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=30, choices=image_filter_choices, default='normal'
     )
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
