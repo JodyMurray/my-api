6998f2c5c01b9e4ddd56b543017b25f5bf0b467b
Jody M <jodyamm@hotmail.com>
2023-01-31 08:32:42 +0000

    Add and migrate reply model

--- reply/models.py
+++ reply/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Reply(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    content = models.TextField()
+    post = models.ForeignKey(
+        Post, related_name='replies', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(
+        User, related_name='likes', blank=True)
+    dislike = models.ManyToManyField(
+        User, related_name='dislike', blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
---
+++ reply/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Reply
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Reply
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
+        ]
