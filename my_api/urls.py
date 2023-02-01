70940af8ad572926ffb56a874d0b3c0b53c2f9e3
Jody M <jodyamm@hotmail.com>
2023-02-01 11:20:06 +0000

    Start generic views for votes app and add urls

--- my_api/urls.py
+++ my_api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('reply.urls')),
+    path('', include('votes.urls')),
 ]
--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from my_api.permissions import IsOwnerOrReadOnly
+from votes.models import Vote
+from votes.serializers import VoteSerializer
 
-# Create your views here.
+
+class VoteList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = VoteSerializer
+    queryset = Vote.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class VoteDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = VoteSerializer
+    queryset = Vote.objects.all()
