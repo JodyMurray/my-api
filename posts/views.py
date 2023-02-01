5c6ae4168308e9a3d4e30df0f2ed487480cfd6d1
Jody M <jodyamm@hotmail.com>
2023-02-01 19:00:16 +0000

    Filters added to posts to include votes and reply count

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from my_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -7,7 +8,21 @@ from .serializers import PostSerializer
 class PostsList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        votes_count=Count('votes', distinct=True),
+        downvotes_count=Count('downvotes', distinct=True),
+        reply_count=Count('replies', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'votes_count',
+        'downvotes_count',
+        'reply_count',
+        'votes__created_at',
+        'downvotes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
