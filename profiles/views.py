75bf6407754195fa85d23c4da5d90c0c846d02a4
Jody M <jodyamm@hotmail.com>
2023-02-06 13:56:58 +0000

    Add owner__followed field in filterset fields

--- profiles/views.py
+++ profiles/views.py
@@ -19,6 +19,7 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'posts_count',
