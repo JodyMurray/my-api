a598702b8cdae1d7a68bd51e20a458e1d9af78c5
Jody M <jodyamm@hotmail.com>
2023-02-06 15:25:53 +0000

    Change settings for date format

--- my_api/settings.py
+++ my_api/settings.py
@@ -23,8 +23,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %b %Y',
+    'DATETIME_FORMAT': '%d %b %Y' + ', %H hours ago',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -127,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': (
             'django.contrib.auth.password_validation.'
             'UserAttributeSimilarityValidator'
-            ),
+        ),
     },
     {
         'NAME': (
