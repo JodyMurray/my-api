b38a60c9617403cac0e19dd923dce76408149761
Jody M <jodyamm@hotmail.com>
2023-02-06 14:35:42 +0000

    Add JWT tokens to settings.py and create serializers.py for currentuser

--- my_api/settings.py
+++ my_api/settings.py
@@ -14,6 +14,22 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'my_api.serializers.CurrentUserSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -42,6 +58,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'posts',
     'profiles',
     'reply',
@@ -50,6 +73,7 @@ INSTALLED_APPS = [
     'followers',
     'saved',
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- reply/views.py
+++ reply/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from my_api.permissions import IsOwnerOrReadOnly
 from .models import Reply
 from .serializers import ReplySerializer, ReplyDetailSerializer
@@ -9,6 +10,8 @@ class ReplyList(generics.ListCreateAPIView):
     serializer_class = ReplySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Reply.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
