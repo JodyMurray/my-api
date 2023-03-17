b77eaf4d202898689b3d39a52d34197541919393
Jody M <jodyamm@hotmail.com>
2023-03-17 10:21:08 +0000

    Create tests for posts app

--- posts/tests.py
+++ posts/tests.py
@@ -2,3 +2,27 @@ from django.contrib.auth.models import User
 from .models import Post
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+
+class PostsListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='jody', password='pass')
+
+    def test_can_list_posts(self):
+        jody = User.objects.get(username='jody')
+        Post.objects.create(owner=jody, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='jody', password='pass')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
