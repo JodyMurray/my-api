4dd5ceee8d256900d7be5c49f0d7da40b3685a93
Jody M <jodyamm@hotmail.com>
2023-03-17 14:09:34 +0000

    Add tests for updating own post, user can't update another user's post

--- posts/tests.py
+++ posts/tests.py
@@ -42,3 +42,18 @@ class PostDetailViewTests(APITestCase):
         self.assertEqual(response.data['title'], 'a title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_can_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='jody', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='ciara', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
